8b78e21f74743fb6af05ad84cedb115f70d29a25
Jakub Grzymisławski <thegomel10@gmail.com>
2020-04-05 22:13:56 +0200

    Change fonts and some images, finish first sections

--- src/sass/components/nav/nav.js
+++ src/sass/components/nav/nav.js
@@ -1,7 +1,26 @@
-const burger = document.querySelector('.nav__hamburger');
-const menu = document.querySelector('.nav__list');
+//DOM elements 
+const elements = {
+    burger: document.querySelector('.nav__hamburger'),
+    menu: document.querySelector('.nav__list'),
+    scrollDown: document.querySelector('.header__scrollDown'),
+    firstSection: document.querySelector('.section--our-top'),
+    navBar: document.querySelector('.nav'),
+}
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('nav__hamburger--active');
-    menu.classList.toggle('nav__list--active');
+const firstSectionOffset = elements.firstSection.offsetTop;
+const navBarHeight = elements.navBar.clientHeight;
+
+//Show menu
+elements.burger.addEventListener('click', () => {
+    elements.burger.classList.toggle('nav__hamburger--active');
+    elements.menu.classList.toggle('nav__list--active');
+})
+
+//Scroll to first section
+elements.scrollDown.addEventListener('click', () => {
+    window.scrollTo({
+        top: firstSectionOffset - navBarHeight,
+        left: 0,
+        behavior: 'smooth'
+    });
 })
