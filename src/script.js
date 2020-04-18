41fc594512290d7e903c9a3dcd00fe389b2e1176
Jakub Grzymisławski <thegomel10@gmail.com>
2020-04-18 17:42:36 +0200

    Scss structure and BEM changes, new photos, Completing the map and footer

--- src/script.js
+++ src/script.js
@@ -5,15 +5,19 @@ const elements = {
     scrollDown: document.querySelector('.header__scrollDown'),
     firstSection: document.querySelector('.section--our-top'),
     navBar: document.querySelector('.nav'),
+    formButton: document.querySelector('.form__button'),
 }
 
 const firstSectionOffset = elements.firstSection.offsetTop;
 const navBarHeight = elements.navBar.clientHeight;
 
+elements.formButton.addEventListener('click', e => e.preventDefault());
+
 //Show menu
 elements.burger.addEventListener('click', () => {
     elements.burger.classList.toggle('nav__hamburger--active');
     elements.menu.classList.toggle('nav__list--active');
+    elements.burger.toggleAttribute('aria-label', 'close menu');
 })
 
 //Scroll to first section
@@ -23,4 +27,12 @@ elements.scrollDown.addEventListener('click', () => {
         left: 0,
         behavior: 'smooth'
     });
+})
+
+lightbox.option({
+    'resizeDuration': 300,
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices': true,
+    'disableScrolling': true,
+    'wrapAround': true
 })
